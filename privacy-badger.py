d56f31150df58ffb6d553402e491306823cd7057
David Mougeolle <moodule@protonmail.com>
2020-02-16 21:07:06 +0100

    Extract blocked domain from a json dump

---
+++ privacy-badger.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+==============
+Privacy-Badger
+==============
+
+Pipe the domain blocked by your privacy-badger.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import json
+
+#####################################################################
+#
+#####################################################################
+
+def extract_blocked_hosts(
+        data: str) -> list:
+    """
+    Extract the blocked hosts from a privacy-badger export
+
+    Parameters
+    ----------
+    data: str.
+        The exported data, as a json string.
+
+    Returns
+    -------
+    out: list.
+        The list of blocked domains.
+    """
+    __structured_data = json.loads(data)
+    return [
+        __domain
+        for __domain, __action
+        in __structured_data.get("action_map", {}).items()
+        if __action["heuristicAction"] == u"block"]
